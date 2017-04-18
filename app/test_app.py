875f1d4e4eb5eba890ac06a56b56d5a6458cb901
Diego Ubirajara <diego.ubirajara@mail.com>
2017-04-18 19:06:03 +0200

    update test

--- app/test_app.py
+++ app/test_app.py
@@ -10,14 +10,18 @@ class FlaskAppTestCase(unittest.TestCase):
         self.client = app.test_client()
         self.index = self.client.get('/')
         self.api = self.client.post('/api/slackbot')
+        self.api_get = self.client.get('/api/slackbot')
 
     def test_get_index(self):
         self.assertEqual(self.index.status_code, 200)
 
-    def test_get_invite(self):
+    def test_post_api(self):
         self.assertEqual(self.api.status_code, 200)
 
-    def test_content_type(self):
+    def test_get_api(self):
+        self.assertEqual(self.api_get.status_code, 405)
+
+    def test_content_type_index(self):
         self.assertIn('text/html', self.index.headers['Content-Type'])
 
 
