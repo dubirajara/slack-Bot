994764a539289ebd5510b83ca73f6dd8b13fbeb3
Diego Ubirajara <diego.ubirajara@mail.com>
2017-04-27 19:38:19 +0200

    update test

--- app/test_app.py
+++ app/test_app.py
@@ -10,7 +10,6 @@ class FlaskAppTestCase(unittest.TestCase):
         self.client = app.test_client()
         self.index = self.client.get('/')
         self.api = self.client.post('/api/slackbot')
-        self.api_get = self.client.get('/api/slackbot')
 
     def test_get_index(self):
         self.assertEqual(self.index.status_code, 200)
@@ -19,7 +18,12 @@ class FlaskAppTestCase(unittest.TestCase):
         self.assertEqual(self.api.status_code, 200)
 
     def test_get_api(self):
-        self.assertEqual(self.api_get.status_code, 405)
+        api_get = self.client.get('/api/slackbot')
+        self.assertEqual(api_get.status_code, 405)
+
+    def test_get_not_found(self):
+        resp = self.client.get("/not-found")
+        self.assertEqual(resp.status_code, 404)
 
     def test_content_type_index(self):
         self.assertIn('text/html', self.index.headers['Content-Type'])
