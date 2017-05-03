c1f052a0ae51daa3e6c3fc6bd1ad7a60553deb96
Diego Ubirajara <diego.ubirajara@mail.com>
2017-05-03 19:38:53 +0200

    update test

--- app/test_app.py
+++ app/test_app.py
@@ -12,19 +12,23 @@ class FlaskAppTestCase(unittest.TestCase):
         self.api = self.client.post('/api/slackbot')
 
     def test_get_index(self):
+        '''Get index must return status code 200'''
         self.assertEqual(self.index.status_code, 200)
 
     def test_post_api(self):
+        '''post api endpont slack must return status code 200'''
         self.assertEqual(self.api.status_code, 200)
 
     def test_index_context(self):
         self.assertIn(b'python <b>madrid</b> learn</h3>', self.index.data)
 
     def test_get_api(self):
+        '''Get api endpont slack must return status code 405'''
         api_get = self.client.get('/api/slackbot')
         self.assertEqual(api_get.status_code, 405)
 
     def test_get_not_found(self):
+        '''Get page not found must return status code 404'''
         resp = self.client.get("/not-found")
         self.assertEqual(resp.status_code, 404)
 
