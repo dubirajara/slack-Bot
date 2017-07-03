c802c7fc12c302be7b1600da25503280c7559559
Diego Ubirajara <diego.ubirajara@mail.com>
2017-07-03 19:19:43 +0200

    update test

--- app/test_app.py
+++ app/test_app.py
@@ -1,34 +1,49 @@
 # coding: utf-8
-
+import json
 from app import app
+from models import db, Slack
 import unittest
 
 
 class FlaskAppTestCase(unittest.TestCase):
 
+
     def setUp(self):
         self.client = app.test_client()
+        db.create_all()
+        self.content = Slack(
+            username='testuser',
+            content='hello word, this is a test',
+            channel='SlackTest',
+            channel_id='TEST123',
+            timestamp=122344557
+        )
+        db.session.add(self.content)
+        db.session.commit()
         self.index = self.client.get('/')
         self.api = self.client.post('/api/slackbot')
 
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_get_user(self):
+        self.assertEqual(self.content.username, 'testuser')
+
     def test_get_index(self):
-        '''Get index must return status code 200'''
         self.assertEqual(self.index.status_code, 200)
 
     def test_post_api(self):
-        '''post api endpont slack must return status code 200'''
         self.assertEqual(self.api.status_code, 200)
 
     def test_index_context(self):
         self.assertIn(b'python <b>madrid</b> learn</h3>', self.index.data)
 
     def test_get_api(self):
-        '''Get api endpont slack must return status code 405'''
         api_get = self.client.get('/api/slackbot')
         self.assertEqual(api_get.status_code, 405)
 
     def test_get_not_found(self):
-        '''Get page not found must return status code 404'''
         resp = self.client.get("/not-found")
         self.assertEqual(resp.status_code, 404)
 
